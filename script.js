ee2dfe4f8d66080e907db2e014c7dd46812b13a5
Vanessa-D <vanessa.ducloux@laposte.net>
2021-04-28 15:45:42 +0200

    first

---
+++ script.js
@@ -0,0 +1,10 @@
+// Manipuler les styles CSS --> méthode css()
+
+$(document).ready(function(){
+    $("ol li").css("color", "dodgerblue");
+});
+
+
+
+// OR
+// $("ol > li").css('color', 'red'); // Indique que li est l'enfant de ol
